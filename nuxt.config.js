a3278927a5667c1510536bb63937e2f36a599630
Christian Lee <cllz@amazon.com>
2021-12-18 01:29:55 -0500

    Fix pre-rendering configurations

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 export default {
-  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+  // Enable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: true,
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,106 @@
 <template>
-  <HomePage />
+    <div class="text-gray-400 whitespace-pre-line leading-tight">
+        <p>&lt;!DOCTYPE html&gt;</p>
+        <p>&lt;html&gt;</p>
+        <div class="ml-4">
+            <p>&lt;head&gt;</p>
+            <div class="ml-4">
+                <p> &lt;<span class="text-indigo-400">title</span>&gt;<span class="text-black">Christian Lee</span>&lt;/<span class="text-indigo-400">title</span>&gt;
+                    &lt;meta charSet="utf-8"/&gt;
+                    &lt;meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/&gt;
+                    &lt;meta name="generator" content="Gatsby 2.21.1"/&gt;
+                    &lt;link rel="author" title="instagram" href="https://instagram.com/cjl.e/"/&gt;
+                    &lt;link rel="author" title="linkedin" href="https://linkedin.com/in/cjle"/&gt;
+                    &lt;link rel="alternate" type="application/rss+xml" href="/rss.xml"/&gt;
+                    &lt;link rel="sitemap" type="application/xml" href="/sitemap.xml"/&gt;
+                    &lt;link as="script" rel="preload" href="/webpack-runtime-1ff73fa22fa37b2d2a6e.js"/&gt;
+                    &lt;link as="script" rel="preload" href="/framework-f7a21432b9f0980fadac.js"/&gt;
+                </p>
+            </div>
+            <p>&lt;/head&gt;</p>
+            <p>&lt;body&gt;</p>
+                <div class="ml-4">
+                    <p>&lt;div id="layout"&gt;</p>
+                        <div class="ml-4">
+                            <p>&lt;div class="navigation"&gt;</p>
+                            <div class="ml-4">
+                               <p>&lt;div class="navigation__inner"&gt;</p>
+                                    <div class="ml-4">
+                                        <p>&lt;div class="navigation__header"&gt;</p>
+                                            <div class="ml-4">
+                                                <p>&lt;div&gt;</p>
+                                                    <div class="ml-4">
+                                                        <p>&lt;h2 class="navigation__header-title"&gt;</p>
+                                                            <div class="ml-4">
+                                                                <p>&lt;a aria-current="page" class="navigation__header-title-link" href="/"&gt;Home&lt;/div&gt;</p> 
+                                                            </div>
+                                                        <p>&lt;/div&gt;</p> 
+                                                    </div>
+                                                <p>&lt;/div&gt;</p> 
+                                            </div>
+                                        <p>&lt;/div&gt;</p>
+                                        <p>&lt;nav class="menu"&gt;</p>
+                                            <div class="ml-4">
+                                                <p>&lt;ul class="menu__list"&gt;</p>
+                                                    <div class="ml-4">
+                                                        <span v-for="page in pages" :key="page.id">
+                                                            <p>&lt;li class="menu__list-item"&gt;</p>
+                                                                <div class="ml-4">
+                                                                    <p>&lt;<span class="text-indigo-400">a</span> <span class="text-blue-400">class</span>=<span class="text-yellow-500 opacity-70">"menu__list-item-link"</span> <span class="text-blue-400">href</span>=<span class="text-yellow-500 opacity-70">"{{ page.attributes.href }}"</span>&gt;</p>
+                                                                        <div class="ml-4">
+                                                                            <p class="text-black">{{ page.attributes.Title }}</p>
+                                                                        </div>
+                                                                    <p>&lt;/<span class="text-indigo-400">a</span>&gt;</p>
+                                                                </div>
+                                                            <p>&lt;/li&gt;</p>
+                                                        </span>
+                                                    </div>
+                                                <p>&lt;/ul&gt;</p>
+                                            </div>
+                                        <p>&lt;/nav&gt;</p>
+                                    </div>
+                               <p>&lt;/div&gt;</p> 
+                            </div>
+                            <p>&lt;/div&gt;</p>
+                        </div>
+                    <p>&lt;/div&gt;</p>
+                </div>
+            <p>&lt;/body&gt;</p>
+        </div>
+        <p>&lt;/html&gt;</p>
+    </div>
 </template>
+
+<!--
+<template>
+    <div>
+        <div v-if="error">
+            {{ error }}
+        </div>
+        <ul v-else>
+            <li v-for="page in pages" :key="page.id">
+                {{ page.attributes.Title }}
+            </li>
+        </ul>
+    </div>
+</template>
+-->
 
 <script>
 export default {
-  name: 'App',
+    name: 'App',
+    async asyncData (context) {
+        const pageData = await context.$strapi.find('pages', { sort: 'id' });
+        return {
+            pages: pageData.data,
+            error: null
+        }
+    },
+    data () {
+        return {
+            pages: [],
+            error: null
+        }
+    },
 }
 </script>
