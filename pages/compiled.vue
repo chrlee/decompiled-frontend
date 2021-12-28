2618933a3d3a24ad19da67abdf8e8a9414729675
Christian Lee <cllz@amazon.com>
2021-12-28 02:41:28 -0500

    PageLayout component

--- pages/compiled.vue
+++ pages/compiled.vue
@@ -1,9 +1,7 @@
 <template>
     <client-only>
-    <transition-group appear name="fade">
-    <div key="wrapper" class="grid grid-cols-1 md:grid-cols-3 md:h-screen whitespace-pre-line leading-tight inline-block">
-        <div class="pl-8 md:pl-16 pt-8 md:pt-16">
-            <h1 class="text-7xl md:text-5xl tracking-tighter">ㅈㅅ</h1>
+    <page-layout>
+        <template #nav>
             <ul v-for="page in pages" :key="page.id">
                 <li class="text-gray-500 pl-2.5 md:pl-1.5">
                     <a v-if="page.attributes.external" :href="page.attributes.href" target="_blank">
@@ -14,18 +12,19 @@
                     </NuxtLink>
                 </li>
             </ul>
-        </div>
-        <div class="overflow-hidden md:col-span-2 p-8 md:py-16">
+        </template>
+        <div class="overflow-hidden h-screen w-screen p-8 md:py-16">
             <img class="object-contain object-top h-full w-full p-1.5" src="https://aimint.org/ap/wp-content/uploads/sites/18/2016/04/image-placeholder-vertical.jpg" />
         </div>
-    </div>
-    </transition-group>
+    </page-layout>
     </client-only>
 </template>
 
 <script>
+import PageLayout from '~/components/PageLayout.vue';
 export default {
     name: 'App',
+    components: { PageLayout },
     async asyncData (context) {
         const pageData = await context.$strapi.find('pages', { sort: 'id' });
         return {
