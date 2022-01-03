fd012b0d900f362a38a06c0e16359ef25c3988bc
Christian Lee <cllz@amazon.com>
2022-01-03 18:17:45 -0500

    Updated Favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'decompiled-frontend',
+    title: 'christian lee',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
