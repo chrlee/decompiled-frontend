205b5125d8a411adb96d485db0b6f10dbc324f27
Christian Lee <cllz@amazon.com>
2022-02-04 03:41:35 -0500

    Use srcset for responsive homepage image

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-        <img class="object-contain object-top h-fit w-fit" :src="image.url" />
+        <img class="object-contain object-top h-fit w-fit" :src="image.url" :srcset="`${image.formats.small.url} 364w, ${image.formats.medium.url} 545w, ${image.formats.large.url} 727w`" />
 </template>
 
 <script>
