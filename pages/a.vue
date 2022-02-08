1a38a7fdb5bab81baaa0e2cd9998456227ecbcd3
Christian Lee <cllz@amazon.com>
2022-02-07 23:04:08 -0500

    About page

---
+++ pages/a.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="pt-4 justify-center">
+    <p class="font-extralight"> New York, NY. 2022.</p>
+  </div>  
+</template>
+
+<script>
+export default {
+  name: "About"
+}
+</script>
