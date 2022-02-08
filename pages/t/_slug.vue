70990c25cf35e5dc1aff5ce6656a25fdbe175696
Christian Lee <cllz@amazon.com>
2022-02-07 22:59:04 -0500

    Placeholder tech items page

---
+++ pages/t/_slug.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="pt-4 justify-center font-extralight">
+    <NuxtLink to="/t">Tech Items are in progress. Click to return.</NuxtLink>
+  </div>
+</template>
+<script>
+export default {
+  name: "TechItem"
+}
+</script>
